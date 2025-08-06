03cc34686346013cfb43f1dd4a53f3251cb3d02c
Poonam4579 <poonamkaur19757@gmail.com>
2025-08-06 21:15:56 +0530

    add responsivness file

--- file.js
+++ file.js
@@ -112,4 +112,46 @@ timingOFmeal04.addEventListener("click", function () {
   image16.classList.add("showO");
 })
 
-  
+function conditions(event) {
+  event.preventDefault();
+
+  let form_person = document.querySelector('#person');
+  let form_date = document.querySelector('.date-form');
+  let form_time = document.querySelector('.time-form');
+
+  if (form_person.value === '') {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Missing Info',
+      text: 'Please add person count'
+    });
+  }
+  else if (form_date.value === '') {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Missing info',
+      text: 'Please add date'
+    });
+  }
+  else if (form_time.value === '') {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Missing info',
+      text: 'Please add time'
+    });
+  }
+  else {
+    Swal.fire({
+      icon: 'Success',
+      title: 'Booked',
+      text: 'Your table got Booked 😊'
+    });
+  }
+}
+
+let humburger = document.querySelector('.hambarger');
+let navlist = document.querySelector('.all-li');
+
+humburger.addEventListener("click", () => {
+  navlist.classList.toggle('active');
+});
