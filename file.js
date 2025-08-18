3ede4f50a06d7cee58287a601752aa38ef4be422
Poonam4579 <poonamkaur19757@gmail.com>
2025-08-18 19:18:32 +0530

    add changes in order form

--- file.js
+++ file.js
@@ -152,81 +152,89 @@ function conditions(event) {
   }
 }
 
-let confirm_order = document.querySelector('.order-box-btn');
+// let confirm_order = document.querySelector('.order-box-btn');
 
-confirm_order.addEventListener("click", (e) => {
+// confirm_order.addEventListener("click", (e) => {
 
-  e.preventDefault();
+//   e.preventDefault();
 
-  let name = document.querySelector('#name');
-  let num = document.querySelector('#ph-num');
-  let per_count = document.querySelector('#box-person');
-  let date_odr = document.querySelector('.box-date-form');
-  let time_odr = document.querySelector('.box-time-form');
+//   let name = document.querySelector('#name');
+//   let num = document.querySelector('#ph-num');
+//   let per_count = document.querySelector('#box-person');
+//   let date_odr = document.querySelector('.box-date-form');
+//   let time_odr = document.querySelector('.box-time-form');
 
-  if (name.value.trim() === '') {
-    Swal.fire({
-      width: '30%',
-      icon: 'warning',
-      text: 'Enter your Name please!'
-    });
-    return;
-  }
-  else if (num.value.trim() === '') {
-    Swal.fire({
-      width: '30%',
-      icon: 'warning',
-      text: 'Enter your ph.Number please!'
-    });
-    return;
-  }
-  else if (per_count.value.trim() === '') {
-    Swal.fire({
-      width: '30%',
-      icon: 'warning',
-      text: 'add person count please!'
-    });
-    return;
-  }
-  else if (date_odr.value.trim() === '') {
-    Swal.fire({
-      width: '30%',
-      icon: 'warning',
-      text: 'add date please!'
-    });
-    return;
-  }
-  else if (time_odr.value.trim() === '') {
-    Swal.fire({
-      width: '30%',
-      icon: 'warning',
-      text: 'add time please!'
-    });
-    return;
-  }
-  else {
-    Swal.fire({
-      width: '30%',
-      icon: "success",
-      title: "Order Confirmed 😊",
-    });
-    show_odr_box.classList.remove("view");
-  }
+//   if (name.value.trim() === '') {
+//     Swal.fire({
+//       width: '30%',
+//       icon: 'warning',
+//       text: 'Enter your Name please!'
+//     });
+//     return;
+//   }
+//   else if (num.value.trim() === '') {
+//     Swal.fire({
+//       width: '30%',
+//       icon: 'warning',
+//       text: 'Enter your ph.Number please!'
+//     });
+//     return;
+//   }
+//   else if (per_count.value.trim() === '') {
+//     Swal.fire({
+//       width: '30%',
+//       icon: 'warning',
+//       text: 'add person count please!'
+//     });
+//     return;
+//   }
+//   else if (date_odr.value.trim() === '') {
+//     Swal.fire({
+//       width: '30%',
+//       icon: 'warning',
+//       text: 'add date please!'
+//     });
+//     return;
+//   }
+//   else if (time_odr.value.trim() === '') {
+//     Swal.fire({
+//       width: '30%',
+//       icon: 'warning',
+//       text: 'add time please!'
+//     });
+//     return;
+//   }
+//   else {
+//     Swal.fire({
+//       width: '30%',
+//       icon: "success",
+//       title: "Order Confirmed 😊",
+//     });
+//     show_odr_box.classList.remove("view");
+//   }
 
-})
+// });
 
-let online_odr = document.querySelector('.order');
-let show_odr_box = document.querySelector('.order-box');
-let close_box = document.querySelector('.close-btn');
+// let online_odr = document.querySelector('.order');
+// let show_odr_box = document.querySelector('.order-box');
+// let close_box = document.querySelector('.close-btn');
+// let background_blur = document.querySelector('.overlay')
  
-online_odr.addEventListener("click", () => {
-  show_odr_box.classList.add("view");
-});
+// online_odr.addEventListener("click", () => {
+//   show_odr_box.classList.add("view");
+//   background_blur.classList.add("overlay");
+//   background_blur.classList.add("on");
+// });
 
-  close_box.addEventListener("click", () => {
-    show_odr_box.classList.remove("view");
+//   close_box.addEventListener("click", () => {
+//     show_odr_box.classList.remove("view");
+//     background_blur.classList.remove("overlay");
     
-  });
+//   });
+// background_blur.addEventListener("click", () => {
+//   show_odr_box.classList.remove("view");
+//   background_blur.classList.remove("overlay");
+//   })
   
 
 
